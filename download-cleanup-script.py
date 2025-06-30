165961e6b393a4a5eae52b1f607c21c257c09e7a
Guilherme Costa <108747806+guilhermelcosta@users.noreply.github.com>
2025-06-30 19:56:53 -0300

    refactor: refac main file

--- download-cleanup-script.py
+++ download-cleanup-script.py
@@ -3,22 +3,19 @@ import shutil
 import time
 from datetime import datetime
 
-DOWNLOADS_PATH = os.path.expanduser("~/Downloads")
-EXCLUSION_QUEUE_PATH = os.path.join(DOWNLOADS_PATH, "exclusion_queue")
-LOG_PATH = os.path.join(EXCLUSION_QUEUE_PATH, "cleanup_log.txt")
+DOWNLOADS_FOLDER_NAME = "Downloads"
+EXCLUSION_FOLDER_NAME = "exclusion_queue"
+CLEANUP_TXT_NAME      = "cleanup_log.txt"
+DOWNLOADS_PATH        = os.path.expanduser(f"~/{DOWNLOADS_FOLDER_NAME}")
+EXCLUSION_QUEUE_PATH  = os.path.join(DOWNLOADS_PATH, EXCLUSION_FOLDER_NAME)
+LOG_PATH              = os.path.join(EXCLUSION_QUEUE_PATH, CLEANUP_TXT_NAME)
 
 def main():
-
-    # Create exclusion_queue folder if it doesn't exist
     os.makedirs(EXCLUSION_QUEUE_PATH, exist_ok=True)
     write_log(f"Created exclusion_queue folder: {EXCLUSION_QUEUE_PATH}")
     write_log("=== Starting Downloads Cleanup Process ===")
-    write_log("Moving files older than 30 days from Downloads to exclusion_queue")
-
-    move_files    
-    write_log("Deleting files older than 60 days from exclusion_queue")
-    delete_old_files
-
+    move_files()    
+    delete_old_files()
 
 def write_log(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -28,6 +25,10 @@ def write_log(message):
     print(log_message)
 
 def move_files():
+
+    write_log("Moving files older than 30 days from Downloads to exclusion_queue")
+
+
     moved_count = 0
     now = time.time()
     # days_30 = 30 * 24 * 60 * 60
@@ -60,8 +61,12 @@ def move_files():
     write_log(f"Successfully moved {moved_count} files to exclusion_queue")
 
 def delete_old_files():
+
+    write_log("Deleting files older than 60 days from exclusion_queue")
+
     deleted_count = 0
     days_60 = 60
+    now = time.time()
 
     for filename in os.listdir(EXCLUSION_QUEUE_PATH):
         file_path = os.path.join(EXCLUSION_QUEUE_PATH, filename)
